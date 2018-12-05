f41efc7eef99bd74c04ce1647188d8a060fb0b7f
Huu Hoang <hoangduchuuvn@gmail.com>
2018-12-06 01:42:38 +0700

    [DEV] migrate to Android X and config kodein

--- app/src/main/java/grabteacher/com/MainActivity.kt
+++ app/src/main/java/grabteacher/com/MainActivity.kt
@@ -1,12 +1,14 @@
 package grabteacher.com
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.kodein.di.Kodein
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
