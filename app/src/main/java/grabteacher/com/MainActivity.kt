0c53f0ce7365f0bec844bfdb71e6e4f2bfff3d49
Huu Hoang <hoangduchuuvn@gmail.com>
2018-12-14 00:28:35 +0700

    [DEV] make MainActivity extends InjectedAcitivy base class

--- app/src/main/java/grabteacher/com/MainActivity.kt
+++ app/src/main/java/grabteacher/com/MainActivity.kt
@@ -2,8 +2,9 @@ package grabteacher.com
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import grabteacher.com.di.InjectedActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : InjectedActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
