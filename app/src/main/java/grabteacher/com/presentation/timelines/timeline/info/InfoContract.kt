4e4686e07ed20b6a3dc7bc985d20fa5cc464de49
Huu Hoang <hoangduchuuvn@gmail.com>
2018-12-13 16:03:49 +0700

    bug     java.lang.IllegalStateException: Activity has been destroyed

---
+++ app/src/main/java/grabteacher/com/presentation/timelines/timeline/info/InfoContract.kt
@@ -0,0 +1,17 @@
+package grabteacher.com.presentation.timelines.timeline.info;
+
+/**
+ * Created by Huu Hoang on 15/12/2018
+ */
+interface InfoContract {
+    interface View{
+
+    }
+    interface Presenter{
+
+    }
+    interface Navigator{
+
+    }
+
+}
