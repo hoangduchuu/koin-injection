be322607c458a7c003377afe17fb0df521ddbd40
Huu Hoang <hoangduchuuvn@gmail.com>
2018-12-15 11:56:20 +0700

    [DEV] the same with bellow but try to test without name

--- app/src/main/java/grabteacher/com/presentation/timelines/timeline/di/MainModule.kt
+++ app/src/main/java/grabteacher/com/presentation/timelines/timeline/di/MainModule.kt
@@ -9,6 +9,6 @@ import org.kodein.di.generic.provider
 /**
  * Created by Huu Hoang on 14/12/2018
  */
-fun mainModule() = Kodein.Module("MainActivityModuleDI") {
+fun mainModule() = Kodein.Module {
     bind<MainContract.Presenter>() with provider { MainPresenter() }
 }
