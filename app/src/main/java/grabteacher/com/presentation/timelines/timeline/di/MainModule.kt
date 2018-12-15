d130e89ce590f040d9089bcf2ec01655037239a9
Huu Hoang <hoangduchuuvn@gmail.com>
2018-12-15 13:58:40 +0700

    [DEV] provide UseCase  and use in our Activity

--- app/src/main/java/grabteacher/com/presentation/timelines/timeline/di/MainModule.kt
+++ app/src/main/java/grabteacher/com/presentation/timelines/timeline/di/MainModule.kt
@@ -1,14 +1,19 @@
 package grabteacher.com.presentation.timelines.timeline.di;
 
+import grabteacher.com.interactor.mainUseCase.FirstUseCase
 import grabteacher.com.presentation.timelines.timeline.view.MainPresenter
 import grabteacher.com.presentation.timelines.timeline.view.MainContract
 import org.kodein.di.Kodein
 import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
 import org.kodein.di.generic.provider
 
 /**
  * Created by Huu Hoang on 14/12/2018
  */
 fun mainModule() = Kodein.Module("MainActivityModuleDI") {
-    bind<MainContract.Presenter>() with provider { MainPresenter() }
+    bind<MainContract.Presenter>() with provider { MainPresenter(instance()) }
+
+    bind<FirstUseCase> () with provider { FirstUseCase(instance(), instance()) }
+
 }
