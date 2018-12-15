290f5ecf9e0c331932e7ebc6da9fdb082f019d17
Huu Hoang <hoangduchuuvn@gmail.com>
2018-12-15 11:26:12 +0700

    [DEV] create mainModule, which class provide any what Object will be use in MainModule

---
+++ app/src/main/java/grabteacher/com/presentation/timelines/timeline/di/MainModule.kt
@@ -0,0 +1,14 @@
+package grabteacher.com.presentation.timelines.timeline.di;
+
+import grabteacher.com.presentation.timelines.timeline.view.MainPresenter
+import grabteacher.com.presentation.timelines.timeline.view.MainContract
+import org.kodein.di.Kodein
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.provider
+
+/**
+ * Created by Huu Hoang on 14/12/2018
+ */
+fun mainModule() = Kodein.Module("MainActivityModuleDI") {
+    bind<MainContract.Presenter>() with provider { MainPresenter() }
+}
