e5bfb4baf4b690b6cac6c12a4cae00cf3283c590
Huu Hoang <hoangduchuuvn@gmail.com>
2018-12-15 11:24:21 +0700

    [DEV] provide Context in appmodule

--- app/src/main/java/grabteacher/com/di/AppModule.kt
+++ app/src/main/java/grabteacher/com/di/AppModule.kt
@@ -5,13 +5,15 @@ import grabteacher.com.logger.AndroidLogger
 import grabteacher.com.logger.Logger
 import org.kodein.di.Kodein
 import org.kodein.di.generic.bind
+import org.kodein.di.generic.provider
 import org.kodein.di.generic.singleton
 
 /**
  * Created by Huu Hoang on 06/12/2018
  */
-fun AppModule(context: Context) = Kodein.Module{
-
+fun AppModule(context: Context) = Kodein.Module("appModule"){
+    // TODO remember bind the context over here
+    bind<Context>() with provider { context }
     bind<Logger>() with singleton { AndroidLogger() }
 
 }
