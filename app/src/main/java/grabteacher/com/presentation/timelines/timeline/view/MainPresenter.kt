fd7b061578d05af74c1828529d500dab28a64290
Huu Hoang <hoangduchuuvn@gmail.com>
2018-12-15 13:58:40 +0700

    [DEV] provide UseCase  and use in our Activity

--- app/src/main/java/grabteacher/com/presentation/timelines/timeline/view/MainPresenter.kt
+++ app/src/main/java/grabteacher/com/presentation/timelines/timeline/view/MainPresenter.kt
@@ -1,13 +1,33 @@
 package grabteacher.com.presentation.timelines.timeline.view;
 
 import grabteacher.com.Utils.logs.ALog
+import grabteacher.com.interactor.mainUseCase.FirstUseCase
+import io.reactivex.disposables.Disposable
+import io.reactivex.observers.DisposableObserver
 
 
 /**
  * Created by Huu Hoang on 14/12/2018
  */
-class MainPresenter: MainContract.Presenter {
+class MainPresenter(val firstUseCase: FirstUseCase): MainContract.Presenter {
+
     override fun firstPresenter() {
-        ALog.e("firstPresenter")
+        firstUseCase.run("bla ble", object : DisposableObserver<String>(){
+
+            override fun onComplete() {
+                ALog.e(" complete")
+            }
+
+            override fun onNext(t: String) {
+                ALog.e(" onNext $t")
+
+            }
+
+            override fun onError(e: Throwable) {
+                ALog.e(" onError ${e.localizedMessage}")
+
+            }
+        })
+
     }
 }
