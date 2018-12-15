9e5508aa60de603ef10a24ed5ce264eb16d998a9
Huu Hoang <hoangduchuuvn@gmail.com>
2018-12-15 17:14:21 +0700

    bug     java.lang.IllegalStateException: Activity has been destroyed

--- app/src/main/java/grabteacher/com/presentation/timelines/timeline/di/MainModule.kt
+++ app/src/main/java/grabteacher/com/presentation/timelines/timeline/di/MainModule.kt
@@ -2,22 +2,27 @@ package grabteacher.com.presentation.timelines.timeline.di;
 
 import grabteacher.com.interactor.mainUseCase.FirstUseCase
 import grabteacher.com.interactor.mainUseCase.SecondUseCase
+import grabteacher.com.presentation.timelines.timeline.view.MainActivity
 import grabteacher.com.presentation.timelines.timeline.view.MainPresenter
 import grabteacher.com.presentation.timelines.timeline.view.MainContract
 import grabteacher.com.presentation.timelines.timeline.view.MainPresenterBackup
 import org.kodein.di.Kodein
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
-import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
 
 /**
  * Created by Huu Hoang on 14/12/2018
  */
 fun mainModule() = Kodein.Module("MainActivityModuleDI") {
-    bind<MainContract.Presenter>(tag = "production") with provider { MainPresenter(instance(), instance()) }
-    bind<MainContract.Presenter>(tag = "backup") with provider { MainPresenterBackup(instance(), instance()) }
+    bind<MainContract.Presenter>(tag = "production") with singleton { MainPresenter(instance(), instance(),instance()) }
+    bind<MainContract.Presenter>(tag = "backup") with singleton { MainPresenterBackup(instance(), instance(),instance()) }
 
-    bind<FirstUseCase> () with provider { FirstUseCase(instance(), instance()) }
-    bind<SecondUseCase>() with provider { SecondUseCase(instance(), instance(),instance()) }
+    bind<FirstUseCase> () with singleton { FirstUseCase(instance(), instance()) }
+    bind<SecondUseCase>() with singleton { SecondUseCase(instance(), instance(),instance()) }
+
+
+
+    bind<MainContract.View>() with singleton { MainActivity() }
 
 }
---
+++ app/src/main/java/grabteacher/com/presentation/timelines/timeline/info/InfoNavigator.kt
@@ -0,0 +1,10 @@
+package grabteacher.com.presentation.timelines.timeline.info;
+
+import android.content.Context
+
+/**
+ * Created by Huu Hoang on 15/12/2018
+ */
+class InfoNavigator(val context: Context) : InfoContract.Navigator {
+
+}
